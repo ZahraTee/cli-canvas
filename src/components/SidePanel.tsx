6a617027dc16ae026626b3700ec2e2a8c068ccfc
Zahra Traboulsi <zahratee@gmail.com>
2025-08-31 20:03:06 +0100

    add bold and underline styling

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,13 +1,34 @@
+import { useCurrentEditor } from "@tiptap/react";
+
 export function SidePanel({
   onClickResetContent,
 }: {
   onClickResetContent: () => void;
 }) {
+  const { editor } = useCurrentEditor();
   return (
     <menu className="flex flex-col w-120 h-full p-10 border-l border-l-gray-700 items-center">
-      <button className="btn btn-error" onClick={onClickResetContent}>
-        Reset content to default
-      </button>
+      <div className="flex-1">
+        <div className="flex gap-2">
+          <button
+            className="btn btn-neutral"
+            onClick={() => editor?.chain().focus().toggleBold().run()}
+          >
+            Bold
+          </button>
+          <button
+            className="btn btn-neutral"
+            onClick={() => editor?.chain().focus().toggleUnderline().run()}
+          >
+            Underline
+          </button>
+        </div>
+      </div>
+      <div className="flex-0">
+        <button className="btn btn-error" onClick={onClickResetContent}>
+          Reset content to default
+        </button>
+      </div>
     </menu>
   );
 }
--- src/lib/tiptap/extensions.ts
+++ src/lib/tiptap/extensions.ts
@@ -1,15 +1,22 @@
+import Bold from "@tiptap/extension-bold";
 import { CodeBlock } from "@tiptap/extension-code-block";
 import Document from "@tiptap/extension-document";
 import { Paragraph } from "@tiptap/extension-paragraph";
 import { Text } from "@tiptap/extension-text";
+import { Underline } from "@tiptap/extension-underline";
 
 export const TerminalDocument = Document.extend({
   content: "codeBlock",
 });
 
 export const extensions = [
+  // --------  Nodes --------
   TerminalDocument,
   Paragraph,
   Text,
-  CodeBlock,
+  // By default CodeBlock disallows marks
+  CodeBlock.extend({ marks: "_" }),
+  // -------- Formatting --------
+  Bold,
+  Underline,
 ];
