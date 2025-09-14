74beeea280c868e079955a991ea1915eaae2397a
Zahra Traboulsi <zahratee@gmail.com>
2025-09-14 23:38:11 +0100

    add image export functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useMemo, useRef } from "react";
-import "./styles.css";
-import { Toolbar } from "./components/Toolbar";
-import { TerminalCanvas } from "./components/TerminalCanvas";
-import { SidePanel } from "./components/SidePanel";
 import { EditorContext, useEditor } from "@tiptap/react";
-import { extensions } from "./lib/tiptap/extensions";
-import { DEFAULT_CONTENT, textToEditorContent } from "./lib/tiptap/content";
+import domToImage from "dom-to-image";
+import { useEffect, useMemo, useRef } from "react";
+import { SidePanel } from "./components/SidePanel";
+import { TerminalCanvas } from "./components/TerminalCanvas";
+import { Toolbar } from "./components/Toolbar";
 import { initializeColorVariables } from "./lib/color";
+import { DEFAULT_CONTENT, textToEditorContent } from "./lib/tiptap/content";
+import { extensions } from "./lib/tiptap/extensions";
+import "./styles.css";
 
 export default function App() {
   const canvasRef = useRef<HTMLDivElement | null>(null);
@@ -24,10 +25,24 @@ export default function App() {
     editor.commands.setContent(textToEditorContent(DEFAULT_CONTENT));
   };
 
+  const onClickDownload = async () => {
+    const terminalNode = canvasRef.current?.parentNode;
+
+    if (!terminalNode) {
+      return;
+    }
+
+    const dataUrl = await domToImage.toPng(terminalNode);
+    const downloadLink = document.createElement("a");
+    downloadLink.download = "cli-canvas.png";
+    downloadLink.href = dataUrl;
+    downloadLink.click();
+  };
+
   return (
     <EditorContext.Provider value={editorContext}>
       <main className="flex flex-col column w-full h-full items-center font-mono">
-        <Toolbar />
+        <Toolbar onClickDownload={onClickDownload} />
         <section className="flex h-full w-full flex-1 overflow-hidden">
           <TerminalCanvas ref={canvasRef} />
           <SidePanel onClickResetContent={resetEditorContent} />
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,10 +1,15 @@
-export function Toolbar() {
+export function Toolbar({ onClickDownload }: { onClickDownload: () => void }) {
   return (
     <menu className="navbar border-b border-b-gray-700 min-h-fit px-5 py-3">
       <div className="flex-1">
         <h1 className="text-md before:content-['_>_'] before:text-accent">
           mocli
         </h1>
+      </div>
+      <div>
+        <button className="btn btn-accent btn-sm" onClick={onClickDownload}>
+          Export PNG
+        </button>
       </div>
     </menu>
   );
