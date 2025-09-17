d12be27e7fb06547918ac03543c9a35da143430a
Zahra Traboulsi <zahratee@gmail.com>
2025-09-17 21:08:51 +0100

    add terminal chrome styling and input

--- src/components/TerminalCanvas.tsx
+++ src/components/TerminalCanvas.tsx
@@ -10,22 +10,42 @@ export const TerminalCanvas = React.forwardRef<HTMLDivElement>(
     const { editor } = useCurrentEditor();
 
     return (
-      <div className="overflow-y-auto w-full min-w-50 min-h-100 p-12">
+      <div className="overflow-y-auto w-full min-w-50 p-12">
         <div
-          className="terminal rounded-md"
+          className="terminal rounded-lg"
           style={{
             backgroundColor: `var(${BACKGROUND_COLOR_CSS_VAR})`,
             color: `var(${FOREGROUND_COLOR_CSS_VAR})`,
           }}
         >
-          <div className="rounded-md" />
-          <EditorContent
-            ref={canvasRef}
-            className="canvas w-full caret-accent"
-            editor={editor}
-          />
+          <TerminalChrome />
+          <div className="min-h-100 relative rounded-b-lg border-x border-b border-stone-400 dark:border-stone-800 shadow-lg">
+            <EditorContent
+              ref={canvasRef}
+              className="w-full p-2"
+              editor={editor}
+            />
+          </div>
         </div>
       </div>
     );
   },
 );
+
+const TerminalChrome = () => {
+  return (
+    <div className="relative top-[-0rem] w-full flex items-center justify-center h-[28px] rounded-t-lg bg-stone-100 dark:bg-stone-800 border border-stone-200 dark:border-stone-800">
+      <div className="absolute left-2 flex gap-2">
+        <div className="size-[14px] border border-stone-200 dark:border-stone-800 rounded-4xl bg-red-400" />
+        <div className="size-[14px] border border-stone-200 dark:border-stone-800 rounded-4xl bg-amber-300" />
+        <div className="size-[14px] border border-stone-200 dark:border-stone-800 rounded-4xl bg-green-500" />
+      </div>
+      <div className="w-full flex justify-center">
+        <input
+          defaultValue="mocli"
+          className="font-sans text-stone-600 dark:text-stone-300 text-sm font-semibold text-center w-[calc(100%-148px)]"
+        />
+      </div>
+    </div>
+  );
+};
