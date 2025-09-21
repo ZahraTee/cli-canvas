c7a62a651316ba55a92ce31f3c1fd355128db031
Zahra Traboulsi <zahratee@gmail.com>
2025-09-21 19:41:32 +0100

    set default font size to 14px

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -4,6 +4,6 @@ export function initializeFontVariables() {
   const root = document.documentElement;
   const sheet = document.styleSheets[0];
 
-  root.style.setProperty(FONT_SIZE_CSS_VAR, "16px");
+  root.style.setProperty(FONT_SIZE_CSS_VAR, "14px");
   sheet.insertRule(`.terminal { font-size: var(${FONT_SIZE_CSS_VAR})}`);
 }
